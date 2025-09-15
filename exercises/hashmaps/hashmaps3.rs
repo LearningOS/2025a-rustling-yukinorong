d3af0ada8d279b7c71388e0e897119111e8be4e8
futunan <863347350@qq.com>
2025-09-15 23:37:25 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,23 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
+use std::ops::AddAssign;
 
 // A structure to store the goal details of a team.
+#[derive(Default, Debug, Clone, Copy)] 
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+impl AddAssign for Team {
+    fn add_assign(&mut self, team:Self) {
+        *self = Self {
+            goals_scored: self.goals_scored + team.goals_scored,
+            goals_conceded: self.goals_conceded + team.goals_conceded,
+        }
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +48,38 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // if scores.contains_key(&team_1_name) {
+        //     let team = scores.get_mut(&team_1_name).unwrap();
+        //     team.goals_scored += team_1_score;
+        //     team.goals_conceded += team_2_score;
+        // } else {
+        //     let team = Team {
+        //         goals_scored: team_1_score,
+        //         goals_conceded: team_2_score,
+        //     };
+        //     scores.insert(team_1_name, team);
+        // }
+
+        // if scores.contains_key(&team_2_name) {
+        //     let team = scores.get_mut(&team_2_name).unwrap();
+        //     team.goals_scored += team_2_score;
+        //     team.goals_conceded += team_1_score;
+        // } else {
+        //     let team = Team {
+        //         goals_scored: team_2_score,
+        //         goals_conceded: team_1_score,
+        //     };
+        //     scores.insert(team_2_name, team);
+        // }
+        *scores.entry(team_1_name.to_string()).or_insert_with(Team::default) += Team {
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score,
+        };
+
+        *scores.entry(team_2_name.to_string()).or_insert_with(Team::default) += Team {
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score,
+        };
     }
     scores
 }
